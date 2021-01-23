10c2adb6694935814d86adcef395df042c852dd9
Andriy Savytskyy <andy1978dnepr@gmail.com>
2021-01-23 02:03:39 +0200

    ver 0

---
+++ src/org/example/Main.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.io.File;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please specify the directory to be searched: ");
+        File dir = new File(sc.nextLine());
+        System.out.println("Please specify the name of the file to search for: ");
+        String fileName = sc.nextLine();
+
+        File result = findAbsoluteFile(dir, fileName);
+        if (result != null) {
+            System.out.println("File found at: " + result);
+        } else {
+            System.out.println("File not found");
+        }
+    }
+
+    //TODO МЕТОД ПРОДОЛЖАЕТ ПЕРЕБИРАТЬ ПАПКУ ДАЖЕ ПОСЛЕ ОБНАРУЖЕНИЯ ФАЙЛА, КАК ЕГО ОСТАНОВИТЬ?
+
+    public static File findAbsoluteFile(File dir, String fileName) {
+        File result = null;
+        try {
+            for (File file : dir.listFiles()) {
+                //Search sequence monitor
+                System.out.printf("checking: %s, result = %s%n", file.getAbsolutePath(), result);
+
+                if (file.getName().toLowerCase().equals(fileName.toLowerCase()) && result == null) {
+                    result = file.getAbsoluteFile();
+                    return result;
+                }
+                if (file.isDirectory() && result == null) {
+                    result = findAbsoluteFile(file, fileName);
+                }
+            }
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
